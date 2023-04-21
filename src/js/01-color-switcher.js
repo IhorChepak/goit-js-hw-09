39dccbe657823928c46f1e255c5c516ae0f18548
IhorChepak <chepak_lex@ukr.net>
2023-04-21 12:27:01 +0300

    w

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+let intervalId = null;
+
+function changeBgColor() {
+    const body = document.querySelector('body');
+    const randomColor = getRandomHexColor();
+    body.style.backgroundColor = randomColor;
+}
+
+function startChangeBgColor() {
+    startBtn.disabled = true;
+    intervalId = setInterval(changeBgColor, 1000);
+}
+
+function stopChangeBgColor() {
+    startBtn.disabled = false;
+    clearInterval(intervalId);
+}
+
+startBtn.addEventListener('click', startChangeBgColor);
+stopBtn.addEventListener('click', stopChangeBgColor);
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+}
